5b7c888217d528c73589096ead5ebcb1829744e6
kaua <kauasousaaragao@gmail.com>
2023-10-26 15:28:51 -0300

    tela-cadastro-consulta-jzdq

--- src/ABC---PAGES---ADM/cadastro-produto/index.jsx
+++ src/ABC---PAGES---ADM/cadastro-produto/index.jsx
@@ -3,82 +3,124 @@ import Cabecalho2 from '../../cabecalho2';
 import Cabecalho from '../../cabecalho';
 import { useState } from 'react';
 import axios from 'axios'
-import { ToastContainer,toast } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-export default function CadastroProduto(){
+import HomeAdm from '../../homeadm';
+export default function CadastroProduto() {
 
-const [nomeProduto, setNomeProduto] = useState ('');
-const [codigoCategoria, setCodigoCategoria] = useState ('');
-const [precoVenda , setPrecoVenda] = useState ('');
-const [quantidadeEstoque, setQuantidadeEstoque]= useState ('');
-const [codigoProduto, setCodigoProduto] = useState('')
-const [erro , setErro] = useState([])
+    const [nomeProduto, setNomeProduto] = useState('');
+    const [codigoCategoria, setCodigoCategoria] = useState('');
+    const [precoVenda, setPrecoVenda] = useState('');
+    const [quantidadeEstoque, setQuantidadeEstoque] = useState('');
+    const [codigoProduto, setCodigoProduto] = useState('')
+    const [erro, setErro] = useState([])
 
-async function CadastroProduto () {
+    async function CadastroProduto() {
 
-try {
+        try {
 
-const url = `http://localhost:5001/cadastroproduto`;
-const produto = {
-    nome : nomeProduto ,
-    categoria: codigoCategoria,
-    preco : precoVenda,
-    estoque: quantidadeEstoque,
-    codigo : codigoProduto
-}
+            const url = `http://localhost:5001/cadastroproduto`;
+            const produto = {
+                nome: nomeProduto,
+                categoria: codigoCategoria,
+                preco: precoVenda,
+                estoque: quantidadeEstoque,
+                codigo: codigoProduto
+            }
 
 
-const r = await axios.post(url , produto)
+            const r = await axios.post(url, produto)
 
-toast.dark("✔️ Cadastrado Com Sucesso !")
-} catch (err) {
-    if(err.response.status === 400) {
-        setErro(err.response.data.erro)
-        toast.dark("🔒 Não Foi Possível Cadastrar")
+            toast.dark("✔️ Cadastrado Com Sucesso !")
+        } catch (err) {
+            if (err.response.status === 400) {
+                setErro(err.response.data.erro)
+                toast.dark("🔒 Não Foi Possível Cadastrar")
+            }
+        }
+
     }
-}
-
-}
 
 
 
-    return(
-        <div className='maeeee'>
-            <ToastContainer/>
-             <Cabecalho2/>
+    return (
+        <div className='mae-admm'>
+            <ToastContainer />
 
-            <div className='fundo'>
+            <div className='container'>
 
-            <h1>CADASTRO DE PRODUTOS</h1>
+                <div className="espaco-1">
+                    <div className='logoo'>
+                        <img className='logo' src="./assets/image/logomelhor.png" alt="" />
+                    </div>
+                    <a href='/cadastroproduto' className='agendaa' >
+                        <img className='agenda' src="./assets/image/imgCadastroProduto.png" alt="" />
+                        <h3>CADASTRO</h3>
+                    </a>
 
-                <div className='corpo'>
-                    <div className='varios'>
-                       <input type="text" placeholder='NOME DO PRODUTO' value={nomeProduto} onChange={e => setNomeProduto(e.target.value)} />
-                       <input type="number" placeholder='CÓDIGO DA CATEGORIA' value={codigoCategoria} onChange={e => setCodigoCategoria(e.target.value)}/>
-                       <input type="number" placeholder='PREÇO DE VENDA' value={precoVenda} onChange={e => setPrecoVenda(e.target.value)}/>
-                       <input type="number" placeholder='QUANTIDADE EM ESTOQUE' value={quantidadeEstoque} onChange={e =>  setQuantidadeEstoque(e.target.value)}/>
-                       <input type="number" placeholder='CÓDIGO DO PRODUTO' value={codigoProduto} onChange={e => setCodigoProduto (e.target.value)} />
-                       <div style={{color:"red"}}>{erro}</div>
-                  
+                    <a href='/consultaproduto' className='agendaa'>
+                        <img className='agenda' src="./assets/image/imgLupaConsulta.png" alt="" />
+                        <h3>CONSULTA</h3>
+                    </a>
+                    <div className='agendaa'>
+                        <img className='agenda' src="./assets/image/abcagenda.png" alt="" />
+                        <h3>PEDIDOS</h3>
                     </div>
 
-                    <div className='imagem'>
-                         <img className='nuvem' src="../assets/image/img-cadastro.png" alt="" />
-                             <div className='linha'></div>
-                             <h3 className='letras'>FAÇA O UPLOAD DA IMAGEM OU <br /> ARRASTE AQUI</h3>
-                               <button className='baix'> <img className='baixar' src="../assets/image/dowload-cadastro-img.png" alt="" /></button>
-                    </div>
-                   
                 </div>
 
-                                    <div className='cc'><button className='salvar' onClick={CadastroProduto}>SALVAR</button>  </div>
 
-                                    <div className='rodape'> copyright 2023 POOL MAX | Todos Os Direitos Reservados | Desenvolvido por JKGF</div>
-                
-                
+                <div className="espaco-2">
+
+                    <nav>
+                        <div>
+                            <h2>Seja Bem-Vindo , Admin !!</h2>
+                        </div>
+
+                        <div className='usuario-p'>
+                            <p>NOVOS DADOS</p>
+                            <img className='usuario' src="/assets/image/usuario.png" alt="" />
+                        </div>
+
+                    </nav>
+
+                <div className='d9'>
+                 
+                    <div className='d8'> 
+                        <div className='input-4'>
+                            <input type="text" placeholder='NOME DO PRODUTO' />
+                            <input type="text" placeholder='CATEGORIA'/>
+                            <input type="text" placeholder='VALOR DO PRODUTO'/>
+                            <input type="text" placeholder='QUANTIDADE'/>
+                        </div>
+
+                        <div className='cinzaa'>
+                            <div className='cinza'>
+                                <img src="/assets/image/img-img.png" alt="" />
+                             </div>
+
+                             <div>
+                                <button className='salvar'>SALVAR</button>
+                             </div>
+
+                        </div>
+
+                        
+                    </div>   {/* final da tag "d8" */} 
+                      
+                 <div className='consulta'>
+                    
+                 </div>
+
+                </div> {/* final da tag "d9" */}
+
+                </div>
+
             </div>
 
-           
+
+
+
 
         </div>
 
