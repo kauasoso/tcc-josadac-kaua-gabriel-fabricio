74afe7c0442a4ce81abe487733c33365c02f8430
biel <gaabriel1284@gmail.com>
2023-11-01 18:23:30 -0300

    img carrinho caraio

--- src/produtos/index.jsx
+++ src/produtos/index.jsx
@@ -12,6 +12,8 @@ export default function Produto() {
                 <div className='centralizar'>
                     <h1 className='prd'>FILTROS</h1>
 
+            GABRIELLLLL AAAAAAAAAAAAAAAA👺👹👺👹👹👺👹👹👺👺👹👹👹
+
                     <nav>
                         <h3>Filtros :</h3>
                         <select name="" id="filtro">
